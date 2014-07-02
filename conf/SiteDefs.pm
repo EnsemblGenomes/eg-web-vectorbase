02b58efadceb10f83d8c3c2d4f9e23b5a997f2bd
Nick Langridge <nicklangridge@hotmail.com>
2014-07-02 16:41:38 +0100

    Versions up

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -14,9 +14,9 @@ sub update_conf {
     $SiteDefs::VECTORBASE_BASE_URL  = 'http://www.vectorbase.org';
   }
 
-  $SiteDefs::SITE_RELEASE_VERSION  = '1406';
-  $SiteDefs::SITE_RELEASE_DATE     = 'June 2014';
-  $SiteDefs::VECTORBASE_VERSION    = 'VB-2014-06';
+  $SiteDefs::SITE_RELEASE_VERSION  = '1408';
+  $SiteDefs::SITE_RELEASE_DATE     = 'August 2014';
+  $SiteDefs::VECTORBASE_VERSION    = 'VB-2014-08';
 
   $SiteDefs::ENSEMBL_PORT       = 8080; 
   $SiteDefs::APACHE_BIN         = '/usr/sbin/httpd';
