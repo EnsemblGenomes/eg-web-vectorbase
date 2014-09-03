f20298e0bdbccea27035fcc4144825625fc567ca
Nick Langridge <nicklangridge@users.noreply.github.com>
2014-09-03 17:04:12 +0100

    Fix footer link URLs

--- modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -12,9 +12,9 @@ return qq(
     <div class="twocol-right right">
       <script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script>
        <script type="text/javascript">try { var pageTracker = _gat._getTracker("UA-6417661-1"); pageTracker._trackPageview(); } catch(err) {} </script>
-      <a href="https://preview.vectorbase.org/about">About&nbsp;VectorBase</a> | 
-      <a href="https://preview.vectorbase.org/contact">Contact&nbsp;Us</a> | 
-      <a href="https://preview.vectorbase.org/navigation/help">Help</a> 
+      <a href="/about">About&nbsp;VectorBase</a> | 
+      <a href="/contact">Contact&nbsp;Us</a> | 
+      <a href="/navigation/help">Help</a> 
     </div>); 
 }
 
