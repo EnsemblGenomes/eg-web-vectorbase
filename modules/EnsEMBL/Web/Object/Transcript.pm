58660800b71cb28d81c8e87f629cb7a21189a824
Nick Langridge <nicklangridge@hotmail.com>
2015-10-20 16:46:14 +0100

    VB-4146 account for non-standard probe names

--- modules/EnsEMBL/Web/Object/Transcript.pm
+++ modules/EnsEMBL/Web/Object/Transcript.pm
@@ -49,6 +49,70 @@ sub availability {
   return $self->{'_availability'};
 }
 
+sub get_oligo_probe_data {
+  my $self = shift; 
+  my $fg_db = $self->database('funcgen'); 
+  my $probe_adaptor = $fg_db->get_ProbeAdaptor; 
+  my @transcript_xrefd_probes = @{$probe_adaptor->fetch_all_by_external_name($self->stable_id)};
+  my $probe_set_adaptor = $fg_db->get_ProbeSetAdaptor; 
+  my @transcript_xrefd_probesets = @{$probe_set_adaptor->fetch_all_by_external_name($self->stable_id)};
+  my %probe_data;
+
+  # First retrieve data for Probes linked to transcript
+  foreach my $probe (@transcript_xrefd_probes) {
+    my ($array_name, $probe_name, $vendor, @info);
+   
+    foreach my $complete (@{$probe->get_all_complete_names}) {
+## VB        
+      ($array_name, my @rest) = split /:/,$complete;  
+      $probe_name = join ':', @rest;
+##     
+      $vendor = $_->vendor for(values %{$probe->get_names_Arrays});
+      @info = ('probe', $_->linkage_annotation) for @{$probe->get_all_Transcript_DBEntries};
+
+      my $key = "$vendor $array_name";
+      $key = $vendor if $vendor eq $array_name;
+
+      if (exists $probe_data{$key}) {
+        my %probes = %{$probe_data{$key}};
+        $probes{$probe_name} = \@info;
+        $probe_data{$key} = \%probes;
+      } else {
+        my %probes = ($probe_name, \@info);
+        $probe_data{$key} = \%probes;
+      }
+    }
+  }
+
+  # Next retrieve same information for probesets linked to transcript
+  foreach my $probeset (@transcript_xrefd_probesets) {
+    my ($array_name, $probe_name, $vendor, @info);
+
+    $probe_name = $probeset->name;
+    
+    foreach (@{$probeset->get_all_Arrays}) {
+     $vendor =  $_->vendor;
+     $array_name = $_->name;
+    }
+    
+    @info = ('pset', $_->linkage_annotation) for @{$probeset->get_all_Transcript_DBEntries};
+    
+    my $key = "$vendor $array_name";
+    
+    if (exists $probe_data{$key}){
+      my %probes = %{$probe_data{$key}};
+      $probes{$probe_name} = \@info;
+      $probe_data{$key} = \%probes;
+    } else {
+      my %probes = ($probe_name, \@info);
+      $probe_data{$key} = \%probes;
+    }
+  }
+
+  $self->sort_oligo_data(\%probe_data); 
+}
+
+
 sub sort_oligo_data {
   my ($self, $probe_data) = @_; 
   my $hub        = $self->hub;
@@ -73,12 +137,13 @@ sub sort_oligo_data {
 ## VB 
       if ($array =~ /^AFFY/ and $p_type ne 'pset') { # only show link for AFFY probeset, not the probes
         $text .= $probe_name;
+        $text .= qq{ <span class="small">[$probe_text]</span>} if $probe_text;
       } else {
         $text .= sprintf '<a href="%s/reporter/%s">%s</a>', $self->hub->species_defs->VECTORBASE_EXPRESSION_BROWSER, $probe_name, $probe_name;
+        $text .= qq{ <span class="small">[$probe_text]</span>} if $probe_text;
+        $text .= qq{  [<a href="$url">view all locations</a>]</p>};
       }
 ## 
-      $text .= qq{ <span class="small">[$probe_text]</span>} if $probe_text;
-      $text .= qq{  [<a href="$url">view all locations</a>]</p>};
     }
     
     push @{$self->__data->{'links'}{'ARRAY'}}, [ $array || $array, $text ];
