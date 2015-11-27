8440eb0fbe425cba27156474b27a354f3de781a3
Nick Langridge <nicklangridge@hotmail.com>
2015-11-27 11:00:36 +0000

    Add species and tidy SiteDefs

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -27,51 +27,47 @@ sub update_conf {
   $SiteDefs::ENSEMBL_PRIMARY_SPECIES = 'Anopheles_gambiae';
   $SiteDefs::ENSEMBL_SECONDARY_SPECIES ='Aedes_aegypti';
 
-  map {delete($SiteDefs::__species_aliases{$_}) } keys %SiteDefs::__species_aliases;
-  
-  $SiteDefs::__species_aliases{ 'Aedes_aegypti' } = [qw(aa aaeg aedes )];
-  $SiteDefs::__species_aliases{ 'Culex_quinquefasciatus' } = [qw(cq culex common_house_mosquito Culex_pipiens C.quinquefasciatus C.pipiens )];
-  $SiteDefs::__species_aliases{ 'Ixodes_scapularis'} = [qw(is ixodes blacklegged_tick black_legged_tick I.scapularis)];
-  $SiteDefs::__species_aliases{ 'Pediculus_humanus' } = [qw(ph pediculus body_louse)];
-  $SiteDefs::__species_aliases{ 'Rhodnius_prolixus' } = [qw(rp rhodnius triatomid_bug R.prolixus)];
-  $SiteDefs::__species_aliases{ 'Rhodnius_prolixus' } = [qw(rp rhodnius triatomid_bug R.prolixus)];
-  $SiteDefs::__species_aliases{ 'Phlebotomus_papatasi' } = [qw(pp)];
-  $SiteDefs::__species_aliases{ 'Lutzomyia_longipalpis' } = [qw(ll)];
-  $SiteDefs::__species_aliases{ 'Drosophila_melanogaster' } = [qw(dm)];
-  $SiteDefs::__species_aliases{ 'Biomphalaria_glabrata' } = [qw(bg)];
-  $SiteDefs::__species_aliases{ 'Musca_domestica' } = [qw(md)];
-  $SiteDefs::__species_aliases{ 'Stomoxys_calcitrans' } = [qw(Stomoxys_calcitrans)];
-  
-
-  $SiteDefs::__species_aliases{ 'Anopheles_gambiae' } = [qw(ag agam mosquito mos anopheles)];
-  $SiteDefs::__species_aliases{ 'Anopheles_gambiaeS' } = [qw(ag agam mosquito mos anopheles)];
-  $SiteDefs::__species_aliases{ 'Anopheles_stephensi' } = [qw(as)];
-  $SiteDefs::__species_aliases{ 'Anopheles_darlingi' } = [qw(ad)];
-  $SiteDefs::__species_aliases{ 'Anopheles_albimanus' } = [qw(A.albimanus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_arabiensis' } = [qw(A.arabiensis)];
-  $SiteDefs::__species_aliases{ 'Anopheles_christyi' } = [qw(A.christyi)];
-  $SiteDefs::__species_aliases{ 'Anopheles_dirus' } = [qw(A.dirus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_epiroticus' } = [qw(A.epiroticus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_funestus' } = [qw(A.funestus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_minimus' } = [qw(A.minimus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_quadriannulatus' } = [qw(A.quadriannulatus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_stephensiI' } = [qw(asi)];
-  $SiteDefs::__species_aliases{ 'Anopheles_atroparvus' } = [qw(A.atroparvus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_culicifacies' } = [qw(A.culicifacies)];
-  $SiteDefs::__species_aliases{ 'Anopheles_farauti' } = [qw(A.farauti)];
-  $SiteDefs::__species_aliases{ 'Anopheles_maculatus' } = [qw(A.maculatus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_melas' } = [qw(A.melas)];
-  $SiteDefs::__species_aliases{ 'Anopheles_merus' } = [qw(A.merus)];
-  $SiteDefs::__species_aliases{ 'Anopheles_sinensis' } = [qw(A.sinensis)];
-  $SiteDefs::__species_aliases{ 'Anopheles_sinensisC' } = [qw(A.sinensisC)];  
-  $SiteDefs::__species_aliases{ 'Anopheles_coluzzii' } = [qw(A.coluzzii)];
-
-  $SiteDefs::__species_aliases{ 'Glossina_morsitans' } = [qw(gm glossina tse_tse_fly G.morsitans)];
-  $SiteDefs::__species_aliases{ 'Glossina_austeni' } = [qw(Glossina_austeni)];
-  $SiteDefs::__species_aliases{ 'Glossina_brevipalpis' } = [qw(Glossina_brevipalpis)];
-  $SiteDefs::__species_aliases{ 'Glossina_fuscipes' } = [qw(Glossina_fuscipes)];
-  $SiteDefs::__species_aliases{ 'Glossina_pallidipes' } = [qw(Glossina_pallidipes)];
-  $SiteDefs::__species_aliases{ 'Glossina_palpalis' } = [qw(Glossina_palpalis)];
+  $SiteDefs::ENSEMBL_DATASETS = [qw(
+    Aedes_aegypti
+    Anopheles_albimanus
+    Anopheles_arabiensis
+    Anopheles_atroparvus
+    Anopheles_christyi
+    Anopheles_coluzzii
+    Anopheles_culicifacies
+    Anopheles_darlingi
+    Anopheles_dirus
+    Anopheles_epiroticus
+    Anopheles_farauti
+    Anopheles_funestus
+    Anopheles_gambiae
+    Anopheles_gambiaeS
+    Anopheles_maculatus
+    Anopheles_melas
+    Anopheles_merus
+    Anopheles_minimus
+    Anopheles_quadriannulatus
+    Anopheles_sinensis
+    Anopheles_sinensisC
+    Anopheles_stephensi
+    Anopheles_stephensiI
+    Biomphalaria_glabrata
+    Culex_quinquefasciatus
+    Glossina_austeni
+    Glossina_brevipalpis
+    Glossina_fuscipes
+    Glossina_morsitans
+    Glossina_pallidipes
+    Glossina_palpalis
+    Ixodes_scapularis
+    Lutzomyia_longipalpis
+    Musca_domestica
+    Pediculus_humanus
+    Phlebotomus_papatasi
+    Rhodnius_prolixus
+    Sarcoptes_scabiei
+    Stomoxys_calcitrans
+  )];
 
   @SiteDefs::ENSEMBL_PERL_DIRS    = (
     $SiteDefs::ENSEMBL_WEBROOT.'/perl',
