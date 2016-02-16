8060b630d9448f20ff5af2bf814792c5a9deadb5
Nick Langridge <nicklangridge@hotmail.com>
2016-02-16 15:00:25 +0000

    Better way to override menus

--- modules/EnsEMBL/Web/ImageConfig.pm
+++ modules/EnsEMBL/Web/ImageConfig.pm
@@ -1,79 +1,15 @@
 package EnsEMBL::Web::ImageConfig;
 
 use strict;
+use previous qw(menus);
 
 sub menus {
-  return $_[0]->{'menus'} ||= {
-    # Sequence
-    seq_assembly        => 'Sequence and assembly',
-    sequence            => [ 'Sequence',                'seq_assembly' ],
-    misc_feature        => [ 'Clones & misc. regions',  'seq_assembly' ],
-    genome_attribs      => [ 'Genome attributes',       'seq_assembly' ],
-    marker              => [ 'Markers',                 'seq_assembly' ],
-    simple              => [ 'Simple features',         'seq_assembly' ],
-    ditag               => [ 'Ditag features',          'seq_assembly' ],
-    dna_align_other     => [ 'GRC alignments',          'seq_assembly' ],
-    dna_align_compara   => [ 'Imported alignments',     'seq_assembly' ],
-    
-    # Transcripts/Genes
-    gene_transcript     => 'Genes and transcripts',
-    transcript          => [ 'Genes',                  'gene_transcript' ],    
-    prediction          => [ 'Prediction transcripts', 'gene_transcript' ],
-    lrg                 => [ 'LRG',                    'gene_transcript' ],
-    rnaseq              => [ 'RNASeq models',         'gene_transcript' ],
-    
-    # Supporting evidence
-    splice_sites        => 'Splice sites',
-    evidence            => 'Evidence',
-    
-    # Alignments
-    mrna_prot           => 'mRNA and protein alignments',
-    dna_align_cdna      => [ 'mRNA alignments',    'mrna_prot' ],
-    dna_align_est       => [ 'EST alignments',     'mrna_prot' ],
-    protein_align       => [ 'Protein alignments', 'mrna_prot' ],
-    protein_feature     => [ 'Protein features',   'mrna_prot' ],
-    dna_align_rna       => 'ncRNA',
-## VB
-    rnaseq_align        => 'RNAseq alignments',
-## /VB
-    
-    # Proteins
-    domain              => 'Protein domains',
-    gsv_domain          => 'Protein domains',
-    feature             => 'Protein features',
-    
-    # Variations
-    variation           => 'Variation',
-    recombination       => [ 'Recombination & Accessibility', 'variation' ],
-    somatic             => 'Somatic mutations',
-    ld_population       => 'Population features',
-    
-    # Regulation
-    functional          => 'Regulation',
-    
-    # Compara
-    compara             => 'Comparative genomics',
-    pairwise_blastz     => [ 'BLASTz/LASTz alignments',    'compara' ],
-    pairwise_other      => [ 'Pairwise alignment',         'compara' ],
-    pairwise_tblat      => [ 'Translated blat alignments', 'compara' ],
-    multiple_align      => [ 'Multiple alignments',        'compara' ],
-    conservation        => [ 'Conservation regions',       'compara' ],
-    synteny             => 'Synteny',
-    
-    # Other features
-    repeat              => 'Repeat regions',
-    oligo               => 'Oligo probes',
-    trans_associated    => 'Transcript features',
-    
-    # Info/decorations
-    information         => 'Information',
-    decorations         => 'Additional decorations',
-    other               => 'Additional decorations',
-    
-    # External data
-    user_data           => 'Your data',
-    external_data       => 'External data',
+  my $self  = shift;
+  my $menus = $self->PREV::menus(@_);
+  my $add   = {
+   rnaseq_align => 'RNAseq alignments',
   };
+  return { %$menus, %$add };
 }
 
 sub add_oligo_probes {
