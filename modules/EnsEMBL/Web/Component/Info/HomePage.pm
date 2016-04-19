5e2970d05a20cb1fc870b0b7fbe36be3282f3d0d
Nick Langridge <nicklangridge@hotmail.com>
2016-04-19 13:30:38 +0100

    VB-4686 add link to phenotype list

--- modules/EnsEMBL/Web/Component/Info/HomePage.pm
+++ modules/EnsEMBL/Web/Component/Info/HomePage.pm
@@ -108,4 +108,84 @@ sub _assembly_text {
   return $html;
 }
 
+sub _variation_text {
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $species_defs = $hub->species_defs;
+  my $species      = $hub->species;
+  my $img_url      = $self->img_url;
+  my $sample_data  = $species_defs->SAMPLE_DATA;
+  my $ensembl_version = $species_defs->SITE_RELEASE_VERSION;
+  my $display_name    = $species_defs->SPECIES_SCIENTIFIC_NAME;
+  my $html;
+
+  if ($hub->database('variation')) {
+    $html .= '<div class="homepage-icon">';
+
+    if ($sample_data->{'VARIATION_PARAM'}) {
+      my $var_url  = $species_defs->species_path . '/Variation/Explore?v=' . $sample_data->{'VARIATION_PARAM'};
+      my $var_text = $sample_data->{'VARIATION_TEXT'};
+      $html .= qq(
+        <a class="nodeco _ht" href="$var_url" title="Go to variant $var_text"><img src="${img_url}96/variation.png" class="bordered" /><span>Example variant</span></a>
+      );
+    }
+
+    if ($sample_data->{'PHENOTYPE_PARAM'}) {
+      my $phen_text = $sample_data->{'PHENOTYPE_TEXT'};
+      my $phen_url  = $species_defs->species_path . '/Phenotype/Locations?ph=' . $sample_data->{'PHENOTYPE_PARAM'};
+      $html .= qq(<a class="nodeco _ht" href="$phen_url" title="Go to phenotype $phen_text"><img src="${img_url}96/phenotype.png" class="bordered" /><span>Example phenotype</span></a>);
+    }
+
+    if ($sample_data->{'STRUCTURAL_PARAM'}) {
+      my $struct_text = $sample_data->{'STRUCTURAL_TEXT'};
+      my $struct_url = $species_defs->species_path .'/StructuralVariation/Explore?sv='.$sample_data->{'STRUCTURAL_PARAM'};
+      $html .= qq(<a class="nodeco _ht"  href="$struct_url" title="Go to structural variant $struct_text"><img src="${img_url}96/struct_var.png" class="bordered" /><span>Example structural variant</span></a>);
+    }
+
+    $html .= '</div>';
+    $html .= '<h2>Variation</h2><p><strong>What can I find?</strong> Short sequence variants';
+    if ($species_defs->databases->{'DATABASE_VARIATION'}{'STRUCTURAL_VARIANT_COUNT'}) {
+      $html .= ' and longer structural variants';
+    }
+    if ($sample_data->{'PHENOTYPE_PARAM'}) {
+      $html .= '; disease and other phenotypes';
+    }
+    $html .= '.</p>';
+
+    if ($self->_other_text('variation', $species)) {
+      $html .= qq(<p><a href="/$species/Info/Annotation#variation" class="nodeco"><img src="${img_url}24/info.png" alt="" class="homepage-link" />More about variation in $display_name</a></p>);
+    }
+
+    my $site = $species_defs->ENSEMBL_SITETYPE;
+    $html .= qq(<p><a href="http://ensemblgenomes.org/info/data/variation" class="nodeco"><img src="${img_url}24/info.png" alt="" class="homepage-link" />More about variation in $site</a></p>);
+
+## VB-4686 link to phenotype list
+    $html .= qq(<p><a href="/$species/Phenotype/All" class="nodeco"><img src="${img_url}24/info.png" alt="" class="homepage-link" />List of Phenotypes</a></p>);
+##
+
+    if ($species_defs->ENSEMBL_FTP_URL) {
+      my @links;
+      foreach my $format (qw/gvf vcf/){
+        push(@links, sprintf('<a href="%s/release-%s/%s/%s/" class="nodeco _ht" title="Download (via FTP) all <em>%s</em> variants in %s format">%s</a>', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, $format, lc $species, $display_name, uc $format,uc $format));
+      }
+      push(@links, sprintf('<a href="%s/release-%s/vep/%s_vep_%s_%s.tar.gz" class="nodeco _ht" title="Download (via FTP) all <em>%s</em> variants in VEP format">VEP</a>', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, lc $species, $ensembl_version, $species_defs->ASSEMBLY_NAME, $display_name));
+      my $links = join(" - ", @links);
+      $html .= qq[<p><img src="${img_url}24/download.png" alt="" class="homepage-link" />Download all variants - $links</p>];
+    }
+  }
+  else {
+    $html .= '<h2>Variation</h2><p>This species currently has no variation database. However you can process your own variants using the Variant Effect Predictor:</p>';
+  }
+
+  my $new_vep = $species_defs->ENSEMBL_VEP_ENABLED;
+  $html .= sprintf(
+    qq(<p><a href="%s" class="%snodeco">$self->{'icon'}Variant Effect Predictor<img src="%svep_logo_sm.png" style="vertical-align:top;margin-left:12px" /></a></p>),
+    $hub->url({'__clear' => 1, $new_vep ? qw(type Tools action VEP) : qw(type UserData action UploadVariations)}),
+    $new_vep ? '' : 'modal_link ',
+    $self->img_url
+  );
+
+  return $html;
+}
+
 1;
