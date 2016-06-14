040f6e1259269a93e2221e5580fc553f71bfb32b
Nick Langridge <nicklangridge@hotmail.com>
2016-06-14 09:40:18 +0100

    VB-4845 update serach placeholder text for consistency

--- modules/EnsEMBL/Web/Document/Element/SearchBox.pm
+++ modules/EnsEMBL/Web/Document/Element/SearchBox.pm
@@ -13,7 +13,7 @@ sub content {
       <div id="block-search-form">
         <form id="vb-search" method="get" action="$search_url">
           <input name="site" value="vectorbase" type="hidden">
-          <input class="vb-search-keywords" name="q" value="" size="25" type="text" maxlength="128" placeholder="Search VectorBase" title="Enter the terms you wish to search for." />
+          <input class="vb-search-keywords" name="q" value="" size="25" type="text" maxlength="128" placeholder="Enter search terms" title="Enter the terms you wish to search for." />
           <input class="vb-search-submit" value="Go" type="submit" />
         </form>
         <p><a href="/search/site/%2A?as=True">Advanced search</a></p>
