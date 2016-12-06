d22095ba7d9abc7ca652f299152db0af59897bea
Nick Langridge <nicklangridge@hotmail.com>
2016-12-06 16:53:30 +0000

    VB-5814 update for E86

--- modules/EnsEMBL/Web/Configuration/Gene.pm
+++ modules/EnsEMBL/Web/Configuration/Gene.pm
@@ -17,13 +17,19 @@ sub modify_tree {
   # delete unwanted nodes
   $self->delete_node($_) for (qw(Alleles SecondaryStructure Family ExpressionAtlas));
 
-  # add expression node
-  my $expression_node = $self->create_node('GeneExpressionReporters', 'Expression report',
-    [], { 'availability' => 'gene has_expression', 'url' => $SiteDefs::VECTORBASE_EXPRESSION_BROWSER . "/gene/" . $self->object->param('g'), 'raw' => 1 }
-  );
+  if ($object) {
+    # add expression node
+    my $expression_node = $self->create_node('GeneExpressionReporters', 'Expression report', [], 
+      { 
+        'availability' => 'gene has_expression', 
+        'url' => $SiteDefs::VECTORBASE_EXPRESSION_BROWSER . "/gene/" . $object->param('g'), 
+        'raw' => 1 
+      }
+    );
 
-  my $regulation_node = $self->get_node('Regulation');
-  $regulation_node->after($expression_node);
+    my $regulation_node = $self->get_node('Regulation');
+    $regulation_node->after($expression_node);
+  }
 }
 
 1;
