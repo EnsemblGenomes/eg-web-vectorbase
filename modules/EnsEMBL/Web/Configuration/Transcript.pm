857d07e581d827e5ec0d2d1d76dfec2bed5552b7
Nick Langridge <nicklangridge@hotmail.com>
2016-12-12 13:16:23 +0000

    Remove obsolete variation menu

--- modules/EnsEMBL/Web/Configuration/Transcript.pm
+++ modules/EnsEMBL/Web/Configuration/Transcript.pm
@@ -13,23 +13,6 @@ sub modify_tree {
   $self->delete_node('S4_PROTEIN_STRUCTURE');  
 ##
 
-  # Zoomable variation image
-
-  # $var_menu->append($self->create_node('Variation_Transcript/Image', 'Variation image',
-  #   [qw( variationimage EnsEMBL::Web::Component::Transcript::VariationImage )],
-  #   { 'availability' => 'transcript database:variation core' }
-  # ));
-
-  my $variation_image = $self->get_node('Variation_Transcript/Image');
-  
-  $variation_image->set('components', [qw( 
-    imagetop EnsEMBL::Web::Component::Transcript::VariationImageTop
-    imagenav EnsEMBL::Web::Component::Transcript::VariationImageNav
-    image EnsEMBL::Web::Component::Transcript::VariationImage 
-  )]);
-  
-  $variation_image->set('availability', 'transcript database:variation core');
-
 # EG:ENSEMBL-2785 add this new URL so that the Transcript info appears at the top of the page for the Karyotype display with Locations tables
   my $sim_node = $self->get_node('Similarity');
   $sim_node->append($self->create_subnode('Similarity/Locations', '',
