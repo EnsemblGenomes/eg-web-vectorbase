1200b2fbd0a2a66ee7961f5e91a8b93e4481ed06
Sanjay Boddu <sboddu@ebi.ac.uk>
2018-05-31 16:04:32 +0100

    Ensembl code now uses images from parent directory

--- utils/install.sh
+++ utils/install.sh
@@ -46,6 +46,9 @@ ln -s /vectorbase/ebi/config/charlie/vcf_json eg-web-vectorbase/conf/json
 echo ">> Creating symlinks to species icons. See VB-6568"
 cd eg-web-vectorbase/htdocs/i/species/48/; find ./ -type f -name '[A-Z]*.png' -printf "%f\n" | while read file; do ln -s $file ${file,}; done; cd -;
 
+
+cp eg-web-vectorbase/htdocs/i/species/64/* eg-web-vectorbase/htdocs/i/species/
+
 echo ">> Make tmp dir..."
 
 mkdir tmp
